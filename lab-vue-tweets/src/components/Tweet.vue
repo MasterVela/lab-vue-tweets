541d05ff4917f072df02e47839d2363ddc156bcb
Sergi Vela <93655945+MasterVela@users.noreply.github.com>
2022-09-24 09:25:16 +0200

    manual upload cause I have a problem wth branches

---
+++ lab-vue-tweets/src/components/Tweet.vue
@@ -0,0 +1,47 @@
+<template>
+  <div className="tweet">
+
+    <ProfileImage :user="user" />
+
+    <div className="body">
+      <div className="top">
+        <User :user="user"/>
+        <Timestamp />
+      </div>
+      <Message :message="message"/>
+
+      <Actions />
+    </div>
+
+    <i class="fas fa-ellipsis-h"></i>
+  </div>
+</template>
+
+<script>
+
+import ProfileImage from "../components/ProfileImage.vue"
+import Message from "../components/Message.vue"
+import Timestamp from "../components/Timestamp.vue"
+import Actions from "../components/Actions.vue"
+import User from "../components/User.vue"
+
+export default({
+  name: "Tweet",
+  components: { ProfileImage, Message, Timestamp, Actions, User },
+  data() {
+    return{
+    }
+  },
+  props: {
+    user: Object,
+    message: String,
+  },
+})
+</script>
+
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
